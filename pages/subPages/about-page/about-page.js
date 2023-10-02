57d89a610a47d2d9048ccb071d34b371dbf3f859
Fantasy <1362724990@qq.com>
2023-10-02 12:40:53 +0800

    first commit

---
+++ pages/subPages/about-page/about-page.js
@@ -0,0 +1,26 @@
+Page({
+  data:{
+    info:[
+      {
+        label:'姓名',
+        value:'符芝'
+      },
+      {
+        label: '职业',
+        value: 'web前端'
+      },
+      {
+        label: 'QQ',
+        value: '1362724990'
+      },
+      {
+        label: '微信',
+        value: 'FovZ_011'
+      },
+      {
+        label: 'email',
+        value: 'admin@777nx.cn'
+      }
+    ]
+  }
+})
